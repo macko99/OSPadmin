d056417f22a7b004cf603fc8f16ddfb9f9c2cf69
macko99 <54603685+macko99@users.noreply.github.com>
2020-05-23 21:53:22 +0200

    PDFmaker updated

--- PDFmaker.py
+++ PDFmaker.py
@@ -100,15 +100,15 @@ def makePDF(printed_values, pdf_save_path): #!!!!!!!!!!!!!!!!!!!!!!!!!!! printed
     rep.multi_cell(0.67 * W, 2 * c_height, txt=" SEKCJA:", align='L')
     y_now = rep.y
     c_h_height = c_height
-    heroes = printed_values[11].split("\n", 10)
-    len_sub_5 = min(len(heroes), 5)
-    for h_index in range(len_sub_5):
+    heroes = printed_values[11].split("\n", 8)
+    len_sub_3 = min(len(heroes), 3)
+    for h_index in range(len_sub_3):
         rep.set_xy(W * 0.33 + left_margin, y_now + c_h_height * h_index)
         rep.cell(0.33 * W, c_h_height, txt=" " + heroes[h_index].replace("\n", ", "), align='L')
-    len_sub_rest = min(len(heroes) - 5, 5)
+    len_sub_rest = min(len(heroes) - 3, 3)
     for h_index in range(len_sub_rest):
         rep.set_xy(W * 0.66 + left_margin, y_now + c_h_height * h_index)
-        rep.cell(0.33 * W, c_h_height, txt=" " + heroes[5 + h_index].replace("\n", ", "), align='L')
+        rep.cell(0.33 * W, c_h_height, txt=" " + heroes[3 + h_index].replace("\n", ", "), align='L')
 
     rep.set_xy(W * 0.33 + left_margin, ybefore)
     rep.multi_cell(0.67 * W, 8 * c_height, txt='', align="L", border=1)
@@ -125,15 +125,27 @@ def makePDF(printed_values, pdf_save_path): #!!!!!!!!!!!!!!!!!!!!!!!!!!! printed
     rep.ln(c_height)
 
     ybefore = rep.get_y()
-    rep.multi_cell(0.2 * W, 2 * c_height, txt=" DATA POWROTU:\n " + printed_values[13], align="L", border=1)
+    rep.multi_cell(0.2 * W, 2 * c_height, txt=" DATA POWROTU:\n " + printed_values[13], align="C", border=1)
     rep.set_xy(W * 0.2 + left_margin, ybefore)
-    rep.multi_cell(0.35 * W, 2 * c_height, txt=" GODZ. ZAKONCZ.:    " + printed_values[14], align="L", border=1)
+    rep.multi_cell(0.3 * W, 2 * c_height, txt=" GODZ. ZAKONCZ.:    " + printed_values[14], align="L", border=1)
     rep.set_xy(W * 0.2 + left_margin, ybefore + 2 * c_height)
-    rep.multi_cell(0.35 * W, 2 * c_height, txt=" GODZ. W REMIZIE.:   " + printed_values[15], align="L", border=1)
-    rep.set_xy(W * 0.55 + left_margin, ybefore)
-    rep.multi_cell(0.2 * W, 2 * c_height, txt=" STAN LICZNIKA: \n " + printed_values[16], align="C", border=1)
-    rep.set_xy(W * 0.75 + left_margin, ybefore)
-    rep.multi_cell(0.25 * W, 2 * c_height, txt=" KM. DO MIEJSCA ZD: \n " + printed_values[17], align="C", border=1)
+    rep.multi_cell(0.3 * W, 2 * c_height, txt=" GODZ. W REMIZIE:   " + printed_values[15], align="L", border=1)
+    rep.set_xy(W * 0.5 + left_margin, ybefore)
+    rep.multi_cell(0.15*W, 2 * c_height, txt="ZASTĘP:\n" + printed_values[19], align='C', border=1)
+    rep.set_xy(W * 0.65 + left_margin, ybefore)
+
+    rep.multi_cell(0.15 * W, 4 * c_height, txt="", border=1)
+    rep.set_xy(W * 0.65 + left_margin, ybefore + 0.25 * c_height)
+    rep.multi_cell(0.15 * W, 1 * c_height, txt=" STAN\n LICZNIKA:", align="C")
+    rep.set_xy(W * 0.65 + left_margin, ybefore + 2.5 * c_height)
+    rep.multi_cell(0.15 * W, 1 * c_height, txt=printed_values[16], align="C")
+    rep.set_xy(W * 0.8 + left_margin, ybefore)
+    rep.multi_cell(0.2 * W, 4 * c_height, txt="", border=1)
+    rep.set_xy(W * 0.8 + left_margin, ybefore + 0.25 * c_height)
+    rep.multi_cell(0.2 * W, 1 * c_height, txt=" KM. DO MIEJSCA\nZDARZENIA:", align="C")
+    rep.set_xy(W * 0.8 + left_margin, ybefore + 2.5 * c_height)
+    rep.multi_cell(0.2 * W, 1 * c_height, txt= printed_values[17], align="C")
+
 
     modDate = str(printed_values[18])[11:].replace(":", "")
     path = pdf_save_path + "/" + str(report_no) + "_" + str(printed_values[4]) + "_" + str(printed_values[1]) + "_" + modDate + ".pdf"
